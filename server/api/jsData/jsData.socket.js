64de61d43298e176313eac3847ce3881dcc59da0
Alexander Levin <alevin.ru@gmail.com>
2016-04-16 17:05:48 +0300

    jsData:socket refactor subscriptions

--- server/api/jsData/jsData.socket.js
+++ server/api/jsData/jsData.socket.js
@@ -1,74 +1,72 @@
 'use strict';
-let jsDataModel = require('./jsData.model');
-let debug = require('debug')('sts:jsData.socket');
+
+let debug = require('debug')('sts:jsData:socket');
 let _ = require('lodash');
-var registeredSockets = [];
+let uuid = require('node-uuid');
+let router = require ('./jsData.socket.router');
 
-function handleSuccess(callback) {
-  return reply => {
-    callback({data: reply || []});
-    return reply;
-  }
-}
+var subscriptions = [];
 
-function handleError(callback) {
-  return err => {
-    debug('error occurred', err);
-    callback({error: err})
-  };
-}
+function emitEvent (method, resource, sourceSocketId) {
 
-function unRegister(socket) {
-
-  let sockets = _.filter(registeredSockets, {socket: socket});
-  _.each(sockets, socket => {
-    _.remove(registeredSockets, socket);
-  });
-
-  debug('Registered sockets', registeredSockets.length);
-
-}
-
-function emitEvent(method, resource, sourceSocketId) {
   debug('emitEvent:', method, resource);
+
   return (data) => {
-    _.each(registeredSockets, function (subscriber) {
-      if (_.includes(subscriber.entities, resource)) {
+    _.each(subscriptions, function (subscription) {
+
+      if (_.includes(subscription.filter, resource)) {
+
         let event = 'jsData:' + method;
-        let socket = subscriber.socket;
+        let socket = subscription.socket;
+
         if (socket.id !== sourceSocketId) {
           debug('emitEvent:', event, 'id:', socket.id);
           socket.emit(event, {
             resource: resource,
-            data: _.pick(data,'id')
+            data: _.pick(data, 'id')
           });
         }
+
       }
+
     });
-  }
+  };
+
 }
 
 exports.emitEvent = emitEvent;
 
+function unRegister (socket) {
+
+  let toUnsubscribe = _.filter(subscriptions, {socket: socket});
+
+  debug('unRegister:', socket.id, 'subscriptions:', toUnsubscribe.length);
+
+  _.each(toUnsubscribe, subscription => {
+    _.remove(subscriptions, subscription);
+  });
+
+}
+
+
 exports.register = function (socket) {
 
-  socket.on('jsData:subscribe', function (entities,callback) {
-    debug('subscribe: id:', socket.id, 'entities:', entities);
+  socket.on('jsData:subscribe', function (filter, callback) {
 
-    let idx = _.findIndex(registeredSockets, {socket: socket});
-    if (idx >= 0) {
-      var difference = _.difference(entities, registeredSockets[idx].entities);
-      registeredSockets[idx].entities = registeredSockets[idx].entities.concat(difference);
-    } else {
-      registeredSockets.push({
-        socket: socket,
-        entities: entities
-      });
-    }
+    var subscription = {
+      id: uuid.v4(),
+      socket: socket,
+      filter: filter
+    };
+
+    debug('jsData:subscribe', subscription.id, 'socket:', socket.id, 'filter:', filter);
+
+    subscriptions.push(subscription);
 
     if (_.isFunction(callback)) {
-      callback();
+      callback(subscription.id);
     }
+
   });
 
   socket.on('disconnect', function () {
@@ -78,7 +76,7 @@ exports.register = function (socket) {
   socket.on('jsData', function (data, callback) {
 
     data.options = data.options || {};
-    _.assign(data.options, {
+    _.defaultsDeep(data.options, {
       headers: {
         authorization: socket.accessToken,
         'x-return-post': true
@@ -86,55 +84,9 @@ exports.register = function (socket) {
       sourceSocketId: socket.id
     });
 
-    debug('jsData event', data);
+    //debug('jsData event', data);
 
-    switch (data.method) {
-
-      case 'findAll' :
-      {
-        jsDataModel.findAll(data.resource, data.params, data.options)
-          .then(handleSuccess(callback))
-          .catch(handleError(callback))
-        ;
-        break;
-      }
-      case 'find':
-      {
-        jsDataModel.find(data.resource, data.id, data.options)
-          .then(handleSuccess(callback))
-          .catch(handleError(callback))
-        ;
-        break;
-      }
-      case 'create':
-      {
-        jsDataModel.create(data.resource, data.attrs, data.options)
-          .then(handleSuccess(callback))
-          .catch(handleError(callback))
-        ;
-        break;
-      }
-      case 'update':
-      {
-        jsDataModel.update(data.resource, data.id, data.attrs, data.options)
-          .then(handleSuccess(callback))
-          .catch(handleError(callback))
-        ;
-        break;
-      }
-      case 'destroy':
-      {
-        jsDataModel.destroy(data.resource, data.id, data.options)
-          .then(handleSuccess(callback))
-          .catch(handleError(callback))
-        ;
-        break;
-      }
-      default:
-      {
-        return handleError(callback)(`Unsupported method '${data.method}'`);
-      }
-    }
+    router (data, callback);
 
   });
 
